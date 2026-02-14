8ec9f3be36f18133593cf2e7ee66b002e6a97dc0
Rabeh <rrabih2705@conestogac.on.ca>
2026-02-14 02:57:46 -0500

    Fixed syntax errors in main.c

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -1,49 +1,53 @@
 #include <stdio.h>
 
-void printWelcomeMenu();
-void printOptions();
-void add();
+void printWelcomeMenu(void);
+void printOptions(void);
+void add(void);
 
+int main(void)
+{
+    printWelcomeMenu();
+    printOptions();
 
-void main() {
+    int inputNum;
 
-	printWelcomeMenu();
+    printf("Enter operation number: ");
+    scanf_s("%d", &inputNum);
 
-	printOptions();
+    switch (inputNum)
+    {
+    case 1:
+        add();
+        break;
+    default:
+        printf("Invalid option\n");
+        break;
+    }
 
-	int inputNum;
-
-	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
-
-	switch (inputNum)
-	{
-	case 1:
-		add();
-	}
-
+    return 0;
 }
 
-void printWelcomeMenu() {
-	printf(" **********************\n");
-	printf("**   Welcome to the   **\n");
-	printf("**   BCS Calculator   **\n");
-	printf(" **********************\n");
+void printWelcomeMenu(void)
+{
+    printf(" **********************\n");
+    printf("**   Welcome to the   **\n");
+    printf("**   BCS Calculator   **\n");
+    printf(" **********************\n");
 }
 
-void printOptions() {
-	printf("1. Add\n");
-	printf("2. Subtract\n");
+void printOptions(void)
+{
+    printf("1. Add\n");
+    printf("2. Subtract\n");
 }
 
-void add() {
-	double num1, num2, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	result = num1 + num2;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+void add(void)
+{
+    double num1, num2, result;
+    printf("Enter the first value: ");
+    scanf_s("%lf", &num1);
+    printf("Enter the second value: ");
+    scanf_s("%lf", &num2);
+    result = num1 + num2;
+    printf("%lf + %lf = %lf\n", num1, num2, result);
 }
-
-lsadhjflkjasdlfa
